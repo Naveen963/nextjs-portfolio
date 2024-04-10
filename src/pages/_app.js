6323b594e2e8f733c3554e3dc027641a48d9de0a
Naveen <naveenkumar200101@gmail.com>
2024-04-11 03:43:10 +0530

    home page

---
+++ src/pages/_app.js
@@ -0,0 +1,27 @@
+import "@/styles/globals.css";
+import Head from "next/head";
+import { Montserrat } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const monsterrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main
+        className={`${monsterrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+    </>
+  );
+}
